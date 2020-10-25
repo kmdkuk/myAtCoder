ec26593fd04e0b8b2ccee2d78a9a0557d1f68770
kouki <kouworld0123@gmail.com>
2020-10-26 01:20:14 +0900

    TLE

--- acr106/b/main.py
+++ acr106/b/main.py
@@ -1,3 +1,5 @@
+import itertools
+
 N, M = map(int, input().split())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
@@ -6,16 +8,36 @@ if (M != 0):
 else:
     cd = list(list())
 
-i = 0
-while i < M:
-    diff = b[i] - a[i]
-    a[i] += diff
-    # cd[0]にiがあるか
-    c = [c for c in cd if c[0] == i + 1]
-    if (len(c) != 0):
-        a[c[0][1]-1] -= diff
-    i += 1
-if (a == b):
+groups = list(list())
+for i in range(1, N + 1):
+    conn_list = [item for item in cd if item[0] == i or item[1] == i]
+    if (len(conn_list) == 0):
+        conn_list.append([i])
+    conn_list = list(set(itertools.chain.from_iterable(conn_list)))
+    indexes = [index for index, group in enumerate(
+        groups) if not set(conn_list).isdisjoint(set(group))]
+    if len(indexes) == 0:
+        groups.append([i])
+        indexes = [index for index, group in enumerate(
+            groups) if not set(conn_list).isdisjoint(set(group))]
+    if (len(indexes) > 1):
+        for j in range(len(indexes) - 1, 0, -1):
+            groups[indexes[0]].extend(groups[indexes[j]])
+            groups[indexes[0]] = list(set(groups[indexes[0]]))
+            del groups[indexes[j]]
+    for item in conn_list:
+        groups[indexes[0]].append(item)
+        groups[indexes[0]] = list(set(groups[indexes[0]]))
+
+flag = True
+for group in groups:
+    sum_a = sum([item for index, item in enumerate(a) if index+1 in group])
+    sum_b = sum([item for index, item in enumerate(b) if index+1 in group])
+    if sum_a != sum_b:
+        flag = False
+        break
+
+if (flag):
     print('Yes')
 else:
     print('No')
